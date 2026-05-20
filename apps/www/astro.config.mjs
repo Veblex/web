757d72f725a8a56b52564a9102544c11f6eeb04e
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2026-05-21 00:16:14 +0200

    Potential solution for rendering page with JS disabled. Add OG image tags for link previews. Update link to Moviestix so it goes directly to www. Update meta title and description for home page.

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -3,7 +3,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -29,6 +28,5 @@ export default defineConfig({
         },
     ],
     integrations: [react()],
-    output: "server",
-    adapter: vercel(),
+    output: "static"
 });
